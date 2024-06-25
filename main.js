9fcff2f94ea33a3c39286ac1798d1c40836de850
Ahmed Mostafa <105132890+ahmed-universe@users.noreply.github.com>
2024-06-25 07:00:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const bnfTabs = document.getElementById('bnf-tabs');
+const bnfBackward = document.getElementById('bnf-backward');
+const bnfForward = document.getElementById('bnf-forward');
+
+function computed(elem , prop){
+    return window.getComputedStyle(elem).getPropertyValue(prop);
+}
+
